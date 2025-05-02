e36edf9e45fa539f4c5d976a52e0fec379cde23b
R3VLver9999 <tatkinson1902@gmail.com>
2025-05-02 16:04:11 +0100

    Completed 12-get-articles-by-id-comment-count

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # NC News Seeding
-
+- Link to hosted version at: 
+- The aim of this project is to demonstrate my ability to program the backend of a server host.
+- Once you have cloned this repo there are a few this to set up before you are able to run it.
 - In order for the code to run you must set up .env files. Create two files: '.env.development' and '.env.test' in the main directory.
 - Once you have created each file, in the test file write: 'PGDATABASE = nc_news_test' and in the development file write: 'PGDATABASE = nc_news'.
 - This will allow you to connect to each database locally as well as being able to run the commands specified in the package.json file.
--- test.sql
+++ test.sql
@@ -1,8 +1,12 @@
--- \c nc_news_test;
+\c nc_news_test;
 -- SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.body) AS comment_count
 -- FROM articles
 -- LEFT JOIN comments ON articles.article_id = comments.article_id
 -- GROUP BY articles.article_id
 -- ORDER BY created_at DESC;
 
--- DELETE FROM comments WHERE comment_id = $2 AND article_id = $1; 
+SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.body) AS comment_count
+FROM articles 
+LEFT JOIN comments ON articles.article_id = comments.article_id
+WHERE articles.article_id = 1
+GROUP BY articles.article_id
