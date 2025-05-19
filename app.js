1225b00ec97b85e3dce84cc67a8b7e22853f1450
R3VLver9999 <tatkinson1902@gmail.com>
2025-05-19 09:26:01 +0100

    Added cross origin resource sharing

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ const {
   deleteComment,
   getUsers,
 } = require("./news.controller.js");
+const cors = require('cors');
 
 const db = require("./db/connection.js");
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.get("/api", getApi);
 
